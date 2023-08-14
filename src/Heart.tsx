de60c7a70657dee56de6a84e12cb0599a5fd1722
jankat <cankatguven@gmail.com>
2023-08-14 19:44:22 +0300

    add heart

---
+++ src/Heart.tsx
@@ -0,0 +1,44 @@
+import { BsFillHeartFill } from "react-icons/bs";
+import { useState, useContext, useEffect } from "react";
+import UnsplashContext from "./appContext";
+
+interface Props {
+  imgId: string;
+  largeImg: string;
+  smallImg: string;
+}
+
+const pink = "#ec4899";
+const white = "white";
+
+const Heart: React.FC<Props> = ({ imgId, largeImg, smallImg }) => {
+  const { addGallery, removeGallery, favoryImages } = useContext(UnsplashContext);
+  const [color, setColor] = useState<string>(white);
+
+  const handleColorChange = () => {
+    if (color === white) {
+      addGallery({ id: imgId, urlLarge: largeImg, urlSmall: smallImg });
+    } else {
+      removeGallery(imgId);
+    }
+    setColor((prev) => (prev === pink ? white : pink));
+    
+  };
+
+  useEffect(() => {
+    const favoried = favoryImages.some((item) => {
+      return item.id === imgId;
+    });
+    if (favoried) setColor(pink);
+  }, [color]);
+
+  return (
+    <div
+      onClick={handleColorChange}
+      className="heart-wrapper position-absolute"
+    >
+      <BsFillHeartFill style={{ color }} className="heart" />
+    </div>
+  );
+};
+export default Heart;

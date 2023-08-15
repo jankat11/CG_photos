4f68e80568d4985c728876733e698c3efef9e8a3
jankat <cankatguven@gmail.com>
2023-08-16 00:57:01 +0300

    header links icon add

--- src/Header.tsx
+++ src/Header.tsx
@@ -1,10 +1,11 @@
-import { BsFillSunFill, BsFillMoonFill } from "react-icons/bs";
+import { RiLightbulbFill } from "react-icons/ri";
+import { MdNightlightRound } from "react-icons/md";
+import { IoIosPhotos } from "react-icons/io";
+import { GoHomeFill } from "react-icons/go";
 import { useEffect, useContext } from "react";
 import UnsplashContext from "./appContext";
-import {Image} from "react-bootstrap"
-import photo1 from "./assets/photo1.svg"
-
-
+import { Image } from "react-bootstrap";
+import photo1 from "./assets/photo1.svg";
 
 const Header = () => {
   const { isDark, isMyGalleryOpen, toggleTheme, openGallery, closeGallery } =
@@ -25,28 +26,49 @@ const Header = () => {
 
   return (
     <section className="toggle-container">
-      <Image width={350} className="ms-1 position-absolute icon-img"  src={photo1}  fluid/>
-      <h3 style={{color: isDark ? "white" : "#4a044e"}} className="title jankat mt-1 position-relative">
+      <Image
+        width={350}
+        className="ms-1 position-absolute icon-img"
+        src={photo1}
+        fluid
+      />
+      <h3
+        style={{ color: isDark ? "white" : "#4a044e" }}
+        className="title jankat mt-1 position-relative"
+      >
         Jankat <span className="mx-1">Images</span>
       </h3>
-      <p
-        style={{ color: !isDark ? "#4a044e" : "#e2e8f0" }}
-        onClick={handleGallery}
-        className="gallery-title"
-      >
-        <span style={{ textDecoration: "underline" }}>
-          {!isMyGalleryOpen ? "Gallery" : "home"}
-        </span>
-      </p>
-      <div className="toggle-icon">
-        <div onClick={toggleTheme} className="dark-toggle">
-          {!isDark ? (
-            <BsFillSunFill style={{ color: !isDark ? "#4a044e" : "#e2e8f0" }} />
-          ) : (
-            <BsFillMoonFill />
-          )}
+      <article className="d-flex">
+        <p
+          style={{ color: !isDark ? "#4a044e" : "#e2e8f0" }}
+          onClick={handleGallery}
+          className="gallery-title"
+        >
+          <span>
+            {!isMyGalleryOpen ? (
+              <span>
+                <IoIosPhotos className="me-1" />
+                gallery
+              </span>
+            ) : (
+              <span>
+                <GoHomeFill className="mb-1" /> home
+              </span>
+            )}
+          </span>
+        </p>
+        <div className="toggle-icon">
+          <div onClick={toggleTheme} className="dark-toggle">
+            {!isDark ? (
+              <RiLightbulbFill
+                style={{ color: !isDark ? "#4a044e" : "#e2e8f0" }}
+              />
+            ) : (
+              <MdNightlightRound />
+            )}
+          </div>
         </div>
-      </div>
+      </article>
     </section>
   );
 };

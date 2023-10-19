f869843cebb3e45ae2571a8b0f5ec53a78ea7f86
cankat <cankatguven@gmail.com>
2023-10-19 21:59:32 +0300

    tan dev-tool rm

--- src/main.tsx
+++ src/main.tsx
@@ -1,17 +1,10 @@
 import ReactDOM from "react-dom/client";
 import App from "./App";
 import "./index.css";
-import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { UnsplashContextProvider } from "./appContext";
-import {ReactQueryDevtools} from "@tanstack/react-query-devtools"
-
-const queryClient = new QueryClient();
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <UnsplashContextProvider>
-    <QueryClientProvider client={queryClient}>
-      <App />
-      <ReactQueryDevtools />
-    </QueryClientProvider>
+    <App />
   </UnsplashContextProvider>
 );

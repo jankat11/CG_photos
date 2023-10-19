9ec42b2a011d453fbd53e4a8650998db552bb427
cankat <cankatguven@gmail.com>
2023-10-19 18:36:57 +0300

    rm tanstack

--- src/components/Gallery.tsx
+++ src/components/Gallery.tsx
@@ -1,6 +1,5 @@
-import { useInfiniteQuery } from "@tanstack/react-query";
 import axios from "axios";
-import { useContext, useEffect, useCallback} from "react";
+import { useContext, useEffect, useState, useRef } from "react";
 import UnsplashContext from "../appContext";
 import Spinner from "./Spinner";
 import ImageItem from "./ImageItem";
@@ -21,31 +20,36 @@ const Gallery = () => {
     setTouchFalse,
     nextGalleryPage,
   } = useContext(UnsplashContext);
-  const fetchImages = useCallback(
-    async (pageParam: number) => {
-      const urlParameters = `?per_page=18&page=${pageParam}&query=${
-        searchValue ? searchValue : "beautiful"
-      }`;
-      const { data } = await axios.get(url + urlParameters);
-      setTouchFalse();
-      return data;
-    },
-    [searchValue]
-  );
 
-  const { data, isLoading, fetchNextPage, isFetchingNextPage } =
-    useInfiniteQuery({
-      queryKey: ["photos", searchValue],
-      queryFn: ({ pageParam = 1 }) => fetchImages(pageParam),
-      getNextPageParam: (lastPage, allPages) => {
-        if (lastPage.total_pages > allPages.length) {
-          return allPages.length + 1;
-        }
-      },
+  const [imagesData, setImagesData] = useState<any>(null);
+  const [isLoading, setIsLoading] = useState(false);
+  const reff = useRef(2);
+
+  const fetchImages = async (pageParam: number) => {
+    setIsLoading(true);
+    const urlParameters = `?per_page=18&page=${pageParam}&query=${
+      searchValue ? searchValue : "beautiful"
+    }`;
+    const { data } = await axios.get(url + urlParameters);
+    setTouchFalse();
+    setImagesData((prev) => {
+      if (prev) {
+        return {
+          total: data.total,
+          results: [...prev.results, ...data.results],
+        };
+      } else {
+        return data;
+      }
     });
+    setIsLoading(false);
+    return data;
+  };
 
   const handleFetch = () => {
-    fetchNextPage();
+    fetchImages(reff.current);
+    console.log("ref: ", reff.current, "data is: ", imagesData);
+    reff.current++;
   };
 
   const handleScroll = () => {
@@ -56,7 +60,6 @@ const Gallery = () => {
       if (!isMyGalleryOpen) {
         setTouchTrue();
         handleFetch();
-
         return;
       } else {
         if (favoryImages.length > galleryPage) nextGalleryPage();
@@ -71,9 +74,13 @@ const Gallery = () => {
     };
   }, [isMyGalleryOpen, galleryPage, touch]);
 
-  if (isLoading) {
+  useEffect(() => {
+    fetchImages(1);
+  }, []);
+
+  /*   if (isLoading) {
     return <Spinner />;
-  }
+  } */
 
   if (isMyGalleryOpen) {
     return (
@@ -105,19 +112,17 @@ const Gallery = () => {
 
   return (
     <>
-      {!isLoading &&
-        (parseInt(data?.pages[0].total) !== 0 ? (
+      {!false &&
+        (parseInt(imagesData?.total) !== 0 ? (
           <section className="image-container">
-            {data?.pages.map((pageItem) => {
-              return pageItem.results.map((img: Img) => {
-                return <ImageItem key={img.id} isGallery={false} image={img} />;
-              });
+            {imagesData?.results.map((img: Img) => {
+              return <ImageItem key={img.id} isGallery={false} image={img} />;
             })}
           </section>
         ) : (
           <EmptyInfo result={true} />
         ))}
-      {isFetchingNextPage && <Spinner bottomSpiner={true} />}
+      {/* {isFetchingNextPage && <Spinner bottomSpiner={true} />} */}
     </>
   );
 };
